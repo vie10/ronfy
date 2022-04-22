6f098effe7c4c776353d50a9bdebb91d1b5bfc8a
vie10 <hrekhviktor@gmail.com>
2022-04-22 12:54:34 +0300

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+}
+
+group = "ua.vie10"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.wrapper {
+    version = "7.4.2"
+}
